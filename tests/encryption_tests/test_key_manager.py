e83bd621665f5a24698c138b6e1ecbc5d39016c4
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-09-26 03:16:51 +0400

    Update test_key_manager.py

--- tests/encryption_tests/test_key_manager.py
+++ tests/encryption_tests/test_key_manager.py
@@ -1,9 +1,9 @@
 import pytest
 import os
-from src.encryption.ecdh import X25519Cipher
-from src.encryption.key_manager import KeyManager
 
+from src.adapters.encryption.service import KeyManager
 
 @pytest.fixture
 def key_manager():
     return KeyManager(iterations=60000)
+
