07ea6b728380cc64bf8b8925ccabecfbb933894e
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-09-26 03:13:38 +0400

    Delete src/core directory

--- src/core/dao.py
+++
@@ -1,9 +0,0 @@
-from typing import Awaitable, Optional
-from abc import ABC, abstractmethod
-
-from .dto import LOcalUserDTO, ContactDTO, MessageDTO, EncryptionKeyDTO
-
-class BaseLocalUserGateway(ABC):
-    @abstractmethod
-    async def get_user_by_id(self, user_id: int) -> LocalUserDTO | None:
-        pass
