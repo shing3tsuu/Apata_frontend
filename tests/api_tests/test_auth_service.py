95938454d2e3df9db1c3a86b3cadd8bc3b7f711b
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-10-01 00:04:25 +0400

    Update test_auth_service.py

--- tests/api_tests/test_auth_service.py
+++ tests/api_tests/test_auth_service.py
@@ -11,8 +11,12 @@ from src.adapters.encryption.service import AbstractECDHCipher, AbstractECDSASig
 
 from src.adapters.encryption.service import X25519Cipher, SECP384R1Signature
 
+from src.exceptions import *
+
 number = randint(1, 1000000)
 fake_user = f"fake_user{number}"
+
+ecdsa_private_key = None
 
 async def get_service():
     container = make_async_container(AppProvider())
@@ -30,12 +34,41 @@ async def test_register():
     try:
         data = await auth_service.register(fake_user)
 
-        print(data)
-
         assert data.get('id') is not None
         assert data.get('username') == fake_user
         assert data.get('ecdsa_private_key') is not None
         assert data.get('ecdh_private_key') is not None
+
+        global ecdsa_private_key
+        ecdsa_private_key = data.get('ecdsa_private_key')
+
+        with pytest.raises(UserAlreadyExistsError):
+            await auth_service.register(fake_user) # already exists
+
+        bad_username1 = "" # too short/empty
+
+        with pytest.raises(APIError):
+            await auth_service.register(bad_username1)
+
+        bad_username2 = "a" * 256 # too long
+
+        with pytest.raises(APIError):
+            await auth_service.register(bad_username2)
+
+    finally:
+        await close_container(container)
+
+@pytest.mark.asyncio
+async def test_login():
+    auth_service, container = await get_service()
+
+    try:
+        global ecdsa_private_key
+        data = await auth_service.login(fake_user, ecdsa_private_key)
+
+        assert data.get('access_token') is not None
+
+        assert auth_service.get_session_status()['is_authenticated'] == True
 
     finally:
         await close_container(container)
